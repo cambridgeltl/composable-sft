d11d064168c63612589500a878f699f8c674f908
Alan Ansell <aja63@cam.ac.uk>
2023-02-02 17:08:16 +0000

    add MultiSourceDataset and MultiSourceDataLoader to the top level package

--- src/sft/__init__.py
+++ src/sft/__init__.py
@@ -2,5 +2,11 @@ from .sft import SFT, decode_sparse_tensor, encode_sparse_tensor
 from .sft_args import SftArguments
 from .trainer import SparseFineTuner
 from .lt_sft import LotteryTicketSparseFineTuner
-from .multisource import load_multisource_dataset, load_single_dataset, MultiSourcePlugin
+from .multisource import (
+    load_multisource_dataset,
+    load_single_dataset,
+    MultiSourceDataLoader,
+    MultiSourceDataset,
+    MultiSourcePlugin,
+)
 from .utils import DataCollatorWithConsistentEvalMasking
